681abf786030d39dc539c89be32466cf208fdee6
ko990128@gmail.com <ko990128@gmail.com>
2023-11-19 14:03:51 +0900

    fix scripts for TPT_3D

--- TPT/scripts/test_tpt_3D.sh
+++ TPT/scripts/test_tpt_3D.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-data_root='/hub_data3/byungoh/'
-testsets=$1
-arch=RN50
-# arch=ViT-B/16
-bs=64
-ctx_init=a_photo_of_a
+data_root='/hub_data3/byungoh/modelnet40_ply_hdf5_2048/'
+testsets=ModelNet40
+arch=ViT-B/16
+bs=1
+ctx_init=A_gray,_unclear_3D_model_of
 
-python ./tpt_classification.py ${data_root} --test_sets ${testsets} \
--a ${arch} -b ${bs} --gpu 0 \
---tpt --ctx_init ${ctx_init}
+CUDA_VISIBLE_DEVICES=1 python ./tpt_classification_3D.py \
+    ${data_root} \
+    --test_sets ${testsets} \
+    -a ${arch} \
+    -b ${bs} \
+    --gpu 0 \
+    --tpt \
+    --n_ctx 4 \
+    --ctx_init ${ctx_init} \
+    -p 20 \
+    --selection_p 0.5
